eacb132116c988e40d4f54e6876800fb468eb3b7
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2022-10-27 01:46:57 -0400

    start project and add gitignore

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chasm
+//
+//  Created by Nick Melkadze on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
