36d2f048c149b677f40a2585db50f9aef7f32575
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2022-10-29 13:55:03 -0400

    add your chasm

--- FeedViewController.swift
+++ FeedViewController.swift
@@ -19,14 +19,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var selectedPost: PFObject!
     let myRefreshControl = UIRefreshControl()
     
-    @IBAction func onLogout(_ sender: Any) {
+    /*
         PFUser.logOut()
         let main = UIStoryboard(name: "Main", bundle: nil)
         let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
         
         delegate.window?.rootViewController = loginViewController
-    }
+    */
     
     
     override var inputAccessoryView: UIView?
---
+++ ProfileViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileViewController.swift
+//  Chasm
+//
+//  Created by Nick Melkadze on 10/29/22.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        renderProfile()
+    }
+    
+    func renderProfile() {
+        let user = PFUser.current()!
+        print(user.username!)
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
